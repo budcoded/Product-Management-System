d270e54898541c2812515fa8258774abb82f81c9
budcoded <ajaybishnoi500@gmail.com>
2023-04-14 13:21:07 +0530

    Basic CRUD functionality of Product implemented.

---
+++ src/main/java/com/inventory/productmanagementsystem/Controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.inventory.productmanagementsystem.Controller;
+
+import com.inventory.productmanagementsystem.Model.Product;
+import com.inventory.productmanagementsystem.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "api/product/")
+@ResponseBody
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("getProducts")
+    public List<Product> getAllProducts () {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping("addProduct")
+    public ResponseEntity<String> addProduct (@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+    @DeleteMapping(path = "deleteProduct/{id}")
+    public ResponseEntity<String> deleteProduct (@PathVariable("id") Long productId) {
+        return productService.deleteProduct(productId);
+    }
+
+    @PutMapping(path = "updateProduct/{id}")
+    public ResponseEntity<String> updateProduct (@PathVariable("id") Long productId, @RequestBody Product product) {
+        return productService.updateProduct(productId, product);
+    }
+}
---
+++ src/main/java/com/inventory/productmanagementsystem/Service/ProductService.java
@@ -0,0 +1,56 @@
+package com.inventory.productmanagementsystem.Service;
+
+import com.inventory.productmanagementsystem.Model.Complaint;
+import com.inventory.productmanagementsystem.Model.Product;
+import com.inventory.productmanagementsystem.Repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService (ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts () {
+        return productRepository.findAll();
+    }
+
+    public ResponseEntity<String> addProduct (Product product) {
+        Product savedProduct = productRepository.save(product);
+        if (savedProduct.getName().equals(product.getName())) {
+            return ResponseEntity.ok("Product Successfully Added.");
+        } else {
+            return ResponseEntity.status(400).body("Error in Adding Product.");
+        }
+    }
+
+    public ResponseEntity<String> deleteProduct (Long id) {
+        if (!productRepository.existsById(id)) {
+            return ResponseEntity.status(400).body("Product with id: " + id + " doesn't exist.");
+        } else {
+            productRepository.deleteById(id);
+            return ResponseEntity.status(200).body("Product deleted successfully.");
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<String> updateProduct (Long productId, Product product) {
+        Product updatedProduct = productRepository.findById(productId).orElseThrow(() -> new IllegalStateException("Product with id: " + productId + " doesn't exist."));
+        updatedProduct.setName(product.getName());
+        updatedProduct.setDescription(product.getDescription());
+        updatedProduct.setPrice(product.getPrice());
+        updatedProduct.setQuantity(product.getQuantity());
+        updatedProduct.setProductCategory(product.getProductCategory());
+        updatedProduct.getOrderList().addAll(product.getOrderList());
+        return ResponseEntity.status(200).body("Product updated successfully.");
+    }
+}
