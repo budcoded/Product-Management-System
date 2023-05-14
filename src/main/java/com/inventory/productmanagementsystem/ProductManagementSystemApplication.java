cdccaa19133e15c5c37971bcece093ea61ca0728
budcoded <ajaybishnoi500@gmail.com>
2023-05-14 19:33:05 +0530

    Dummy Commit.

--- src/main/java/com/inventory/productmanagementsystem/ProductManagementSystemApplication.java
+++ src/main/java/com/inventory/productmanagementsystem/ProductManagementSystemApplication.java
@@ -11,5 +11,6 @@ public class ProductManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductManagementSystemApplication.class, args);
+        // Doing dummy commit to check webhook
     }
 }
